357674a1466aa0eb1b0377006415fc9c18b0a131
Jay <jaywu.fe@gmail.com>
2021-10-03 00:58:56 +0800

    rotate swirl

--- src/js/ClassPath.js
+++ src/js/ClassPath.js
@@ -85,28 +85,27 @@ export default class Path {
         this.drawLine(x1, y1, x2, y2)
     }
     drawBezier(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4) {
-        const {x: x1, y: y1} = this.aroundCenterPoint(_x1, _y1, 100)
-        const {x: x2, y: y2} = this.aroundCenterPoint(_x2, _y2, 100)
-        const {x: x3, y: y3} = this.aroundCenterPoint(_x3, _y3, 100)
-        const {x: x4, y: y4} = this.aroundCenterPoint(_x4, _y4, 100)
+        const progress = this.p5.slider.value()
+        const {x: x1, y: y1} = this.rotateCenter(_x1, _y1, progress)
+        const {x: x2, y: y2} = this.rotateCenter(_x2, _y2, progress)
+        const {x: x3, y: y3} = this.rotateCenter(_x3, _y3, progress)
+        const {x: x4, y: y4} = this.rotateCenter(_x4, _y4, progress)
         this.p5.bezier( x1, y1, x2, y2, x3, y3, x4, y4)
     }
     drawLine(_x1, _y1, _x2, _y2) {
-        const {x: x1, y: y1} = this.aroundCenterPoint(_x1, _y1, 100)
-        const {x: x2, y: y2} = this.aroundCenterPoint(_x2, _y2, 100)
+        const progress = this.p5.slider.value()
+        const {x: x1, y: y1} = this.rotateCenter(_x1, _y1, progress)
+        const {x: x2, y: y2} = this.rotateCenter(_x2, _y2, progress)
         this.p5.line(x1, y1, x2, y2)
     }
-    aroundCenterPoint(x, y, r) {
-        const { mouseX, mouseY } = this.p5
-        const mouse = this.p5.createVector(mouseX, mouseY)
-        const center = this.p5.createVector(x, y)
-        const dist = center.dist(mouse)
-        if (dist < r) {
-            center.sub(mouse)
-            center.mult(r/dist)
-            x += center.x
-            y += center.y
-        }
-        return { x, y }
+    rotateCenter(x, y, progress) {
+        const MAX_ANGLE = Math.PI / 2
+        const { createVector, map } = this.p5
+        const center = createVector(350, 450)
+        const node = createVector(x, y)
+        const arrow = node.copy().sub(center)
+        const deg = map(arrow.mag(), 0, 500, 20 * progress + 1, 1)
+        // .sub(node).mult(progress).rotate(deg)
+        return center.add(arrow.rotate(MAX_ANGLE * progress * deg).mult(1 - progress))
     }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,14 @@ const app = document.querySelector('#app');
 new P5(function(p) {
     let fontGenerator;
     let fontPath;
-
+    let slider;
     p.preload = async () => {
         fontGenerator = await initOpentype()
         const path = fontGenerator.getPath('龍大永鬱龘', 100, 500, 100);
-        fontPath = new FontPath(path.commands, p)
+        slider = p.createSlider(0, 1, 0, 0.01);
+        fontPath = new FontPath(path.commands, Object.assign(p, {slider}))
     }
-
+    
     p.setup = ()=> {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
         p.background(100);
@@ -21,6 +22,8 @@ new P5(function(p) {
 
     p.draw = ()=> {
         p.background(100);
+        p.line(350 - 10, 450, 350 + 10, 450);
+        p.line(350, 450 - 10, 350, 450 + 10);
         if(fontPath) {
             fontPath.update();
         }
